27f7f7f59f8fc074189fca63efaceb95f6875cf6
Dmitry Varin <varindv@gmail.com>
2021-10-20 10:33:49 +0300

    fix string after test, add new tests

--- internal/app/models/string.go
+++ internal/app/models/string.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 )
 
-// не очень хорошо, но использовать пакет unsafe не советуют из-за непереносимости..
 const INT32_SIZE = 4
 
 type String struct {
@@ -28,11 +27,11 @@ func (s *String) Encode() ([]byte, error) {
 }
 func (s *String) Decode(binData []byte) error {
 	data := bytes.NewBuffer(binData)
-	if err := binary.Read(data, binary.LittleEndian, s.Len); err != nil {
+	if err := binary.Read(data, binary.LittleEndian, &s.Len); err != nil {
 		return err
 	}
 
-	s.Str = make([]int8, 0, s.Len)
+	s.Str = make([]int8, s.Len, s.Len)
 	for i := int32(0); i < s.Len; i++ {
 		if err := binary.Read(data, binary.LittleEndian, &s.Str[i]); err != nil {
 			return err
--- internal/app/models/string_test.go
+++ internal/app/models/string_test.go
@@ -14,6 +14,18 @@ func convertToProtoString(str string) String {
 	}
 }
 
+func cmp(str String, str2 String) bool {
+	if str.Len != str2.Len || len(str.Str) != len(str.Str) {
+		return false
+	}
+	for i := range str.Str {
+		if str.Str[i] != str2.Str[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestString_GetBytesLength_OrdinaryString(t *testing.T) {
 	testString := "mailisvk"
 	expectedLen := len(testString) + INT32_SIZE
@@ -30,5 +42,55 @@ func TestString_GetBytesLength_EmptyString(t *testing.T) {
 	protoLen := convertToProtoString(testString).GetBytesLength()
 	if protoLen != expectedLen {
 		t.Fatal("incorrect len")
+	}
+}
+func TestString_Encode_Decode(t *testing.T) {
+	testString := "mailisvk"
+	expectedProtoStr := convertToProtoString(testString)
+
+	encoded, err := expectedProtoStr.Encode()
+	if err != nil {
+		t.Fatal("invalid encoded protoStr")
+	}
+	var decodedProtoStr String
+	if err := decodedProtoStr.Decode(encoded); err != nil {
+		t.Fatal("invalid decoded protoStr")
+	}
+	if !cmp(expectedProtoStr, decodedProtoStr) {
+		t.Fatalf("invalid compare: %v not equal %v", expectedProtoStr, decodedProtoStr)
+	}
+}
+func TestString_Encode_Decode_Empty(t *testing.T) {
+	testString := ""
+	expectedProtoStr := convertToProtoString(testString)
+
+	encoded, err := expectedProtoStr.Encode()
+	if err != nil {
+		t.Fatal("invalid encoded protoStr")
+	}
+	var decodedProtoStr String
+	if err := decodedProtoStr.Decode(encoded); err != nil {
+		t.Fatal("invalid decoded protoStr")
+	}
+	if !cmp(expectedProtoStr, decodedProtoStr) {
+		t.Fatalf("invalid compare: %v not equal %v", expectedProtoStr, decodedProtoStr)
+	}
+}
+func TestString_ToString(t *testing.T) {
+	testString := "mailisvk"
+	protoString := convertToProtoString(testString)
+	res, err := protoString.ToString()
+	if err != nil || res != testString {
+		t.Fatalf("invalid convert from protoStr to str\nexpected: %v received: %v",
+			testString, res)
+	}
+}
+func TestString_ToString_Empty(t *testing.T) {
+	testString := ""
+	protoString := convertToProtoString(testString)
+	res, err := protoString.ToString()
+	if err != nil || res != testString {
+		t.Fatalf("invalid convert from protoStr to str\nexpected: %v received: %v",
+			testString, res)
 	}
 }
