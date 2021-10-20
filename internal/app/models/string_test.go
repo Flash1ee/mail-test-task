a0c05a644a2026c974fb6cceeca7e1bf23af5327
Dmitry Varin <varindv@gmail.com>
2021-10-20 10:12:28 +0300

    fix test: mock convert to ProtoString

--- internal/app/models/string_test.go
+++ internal/app/models/string_test.go
@@ -2,11 +2,23 @@ package models
 
 import "testing"
 
+func convertToProtoString(str string) String {
+	length := len(str)
+	b := make([]int8, 0, length)
+	for _, val := range str {
+		b = append(b, int8(val))
+	}
+	return String{
+		Str: b,
+		Len: int32(length),
+	}
+}
+
 func TestString_GetBytesLength_OrdinaryString(t *testing.T) {
 	testString := "mailisvk"
 	expectedLen := len(testString) + INT32_SIZE
 
-	protoLen := GetIprotoString(testString).GetBytesLength()
+	protoLen := convertToProtoString(testString).GetBytesLength()
 	if protoLen != expectedLen {
 		t.Fatal("incorrect len")
 	}
@@ -15,7 +27,7 @@ func TestString_GetBytesLength_EmptyString(t *testing.T) {
 	testString := ""
 	expectedLen := len(testString) + INT32_SIZE
 
-	protoLen := GetIprotoString(testString).GetBytesLength()
+	protoLen := convertToProtoString(testString).GetBytesLength()
 	if protoLen != expectedLen {
 		t.Fatal("incorrect len")
 	}
